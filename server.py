1821b580361f10446c03e18aa95ab2d4eb702d6e
Matt Robenolt <matt@ydekproductions.com>
2013-02-25 02:45:12 -0800

    Refactoring!

--- server.py
+++ server.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 
-import time
-import sys
+from datetime import datetime
 
-from svnserve.protocol import literal, encode, decode
-from svnserve.objects import InMemoryRepository
+from svnserve.protocol import literal, decode
+from svnserve.responses import Response, Success
 from svnserve.exceptions import SvnException, CommandNotImplemented
 
-Repository = InMemoryRepository
-
 # Create and register the testrepo
+from svnserve.objects import InMemoryRepository as Repository
 Repository.create('/testrepo', 'ce6d8bb6-6b7a-4cb9-bf0c-f346bb840b97')
 
 
 class Request(object):
+    MAX_RECV = 1048576  # 1MB
+
     def __init__(self, client):
         self.client = client
+
+    def send(self, data):
+        self.send_raw(str(data))
+
+    def send_raw(self, data):
+        print("\x1b[35m>>> %s\x1b[0m" % data)
+        self.client.send(data)
+
+    def read(self, callback):
+        data = self.client.recv(self.MAX_RECV + 1)
+        if len(data) > self.MAX_RECV:
+            self.send_and_end(SvnException('Message exceeded maximum length: {0} bytes'.format(self.MAX_RECV)))
+            return
+
+        print("\x1b[32m<<< %s\x1b[0m" % data)
+        data = decode(data)
+        try:
+            callback(data)
+        except SvnException as e:
+            self.send_and_end(e)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            self.send_and_end(SvnException('Something unexepcted happened: {0!r}'.format(e)))
 
     def send_and_receive(self, data, callback):
         self.send(data)
@@ -25,44 +49,34 @@ class Request(object):
         self.send(data)
         self.end()
 
-    def send(self, data):
-        if isinstance(data, SvnException):
-            data = data.as_buffer()
-        data = encode(data)
-        self.send_raw(data)
-
-    def send_raw(self, data):
-        print("\x1b[35m>>> %s\x1b[0m" % data)
-        self.client.send(data)
-
-    def read(self, callback):
-        data = self.client.recv(1024)
-        print("\x1b[32m<<< %s\x1b[0m" % data)
-        data = decode(data)
-        try:
-            callback(data)
-        except SvnException as e:
-            self.send_and_end(e)
-        except Exception:
-            self.send_and_end(SvnException('Something unexepcted happened.'))
-
     def handle(self):
+        print(datetime.now().strftime('\n-- %H:%M:%S.%f --'))
         self.greeting()
 
     def greeting(self):
-        self.send_and_receive([literal('success'), [2, 2, [], [literal('edit-pipeline')]]], self.start_auth)
+        self.send_and_receive(
+            Success(2, 2, [], [literal('edit-pipeline')]),
+            self.start_auth,
+        )
 
     def start_auth(self, data):
         # ( success ( ( ANONYMOUS ) 36:40435d67-9594-4fb4-bb67-394e327d0cc5 ) )
         self.request_uri = data[1][2]
         self.repository = Repository.get_by_uri(self.request_uri)
-        self.send_and_receive([literal('success'), [[literal('ANONYMOUS')], self.repository.id]], self.auth2)
+
+        self.send_and_receive(
+            Success([literal('ANONYMOUS')], self.repository.id),
+            self.auth2
+        )
 
     def auth2(self, data):
         # ( success ( ) )
         # ( success ( 36:40435d67-9594-4fb4-bb67-394e327d0cc5 25:svn://localhost:9630/cool ( mergeinfo ) ) )
-        self.send([literal('success'), []])
-        self.send_and_receive([literal('success'), [self.repository.id, self.request_uri, [literal('mergeinfo')]]], self.handle_command)
+        self.send(Success())
+        self.send_and_receive(
+            Success(self.repository.id, self.request_uri, [literal('mergeinfo')]),
+            self.handle_command,
+        )
 
     def handle_command(self, data):
         cmd, args = data[1][0], data[1][1]
@@ -76,24 +90,24 @@ class Request(object):
     def cmd_get_latest_rev(self):
         # ( success ( ( ) 0: ) )
         # ( success ( 0 ) )
-        self.send([literal('success'), [[], '']])
-        self.send_and_receive([literal('success'), [0]], self.handle_command)
+        self.send(Success([], ''))
+        self.send_and_receive(Success(0), self.handle_command)
 
     def cmd_reparent(self, url):
         # ( success ( ( ) 0: ) )
         # ( success ( ) )
-        self.send([literal('success'), [[], '']])
-        self.send_and_receive([literal('success'), []], self.handle_command)
+        self.send(Success([], ''))
+        self.send_and_receive(Success([]), self.handle_command)
 
     def cmd_check_path(self, *args):
         # ( success ( ( ) 0: ) )
         # ( success ( dir ) )
-        self.send([literal('success'), [[], '']])
-        self.send_and_end([literal('success'), [literal('dir')]])
+        self.send(Success([], ''))
+        self.send_and_end(Success(literal('dir')))
 
     def cmd_update(self, *args):
         # ( success ( ( ) 0: ) )
-        self.send_and_receive([literal('success'), [[], '']], self.handle_command)
+        self.send_and_receive(Success([], ''), self.handle_command)
 
     def cmd_set_path(self, *args):
         # ( success ( ( ) 0: ) )
@@ -105,50 +119,40 @@ class Request(object):
         # ( change-dir-prop ( 2:d0 14:svn:entry:uuid ( 36:40435d67-9594-4fb4-bb67-394e327d0cc5 ) ) )
         # ( close-dir ( 2:d0 ) )
         # ( close-edit ( ) )
-        dir_id = 'd0'
-        self.send([literal('success'), [[], '']])
-        self.send([literal('target-rev'), [0]])
-        self.send([literal('open-root'), [[0], dir_id]])
-        self.send([literal('change-dir-prop'), [dir_id, 'svn:entry:committed-rev', ['0']]])
-        self.send([literal('change-dir-prop'), [dir_id, 'svn:entry:committed-date', ['2013-01-16T01:58:37.548920Z']]])
-        self.send([literal('change-dir-prop'), [dir_id, 'svn:entry:last-author', []]])
-        self.send([literal('change-dir-prop'), [dir_id, 'svn:entry:uuid', [self.repository.id]]])
-        self.send([literal('close-dir'), [dir_id]])
-        self.send_and_receive([literal('close-edit'), []], self.handle_command)
+        dir_id = 'd0'  # wtf is this?
+        revision = 0  # Can be any integer, not contiguous or sequential
+        self.send(Success([], ''))
+        self.send(Response('target-rev', revision))
+        self.send(Response('open-root', [revision], dir_id))
+        self.send(Response('change-dir-prop', dir_id, 'svn:entry:committed-rev', [str(revision)]))
+        self.send(Response('change-dir-prop', dir_id, 'svn:entry:committed-date', ['2013-01-16T01:58:37.548920Z']))
+        self.send(Response('change-dir-prop', dir_id, 'svn:entry:last-author', []))
+        self.send(Response('change-dir-prop', dir_id, 'svn:entry:uuid', [self.repository.id]))
+        self.send(Response('close-dir', dir_id))
+        self.send_and_receive(Response('close-edit'), self.handle_command)
 
     def cmd_success(self, *args):
         # ( success ( ) )
-        self.send_and_end([literal('success'), []])
+        self.send_and_end(Success())
 
     def noop(self, data):
         pass
 
     def end(self, *args, **kwargs):
-        # print "=" * 10
         self.client.close()
 
     @classmethod
     def accept(cls, client, address):
         req = cls(client)
         req.handle()
-        req.end()
 
 
 if __name__ == '__main__':
-    from gevent.server import StreamServer
-
-    while 1:
-        try:
-            server = StreamServer(('', 3690), Request.accept)
-            print("listening @ svn://localhost:3690 ...")
-            break
-        except Exception:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            time.sleep(0.5)
+    from svnserve.server import GeventSvnServer
+    server = GeventSvnServer(callback=Request.accept)
 
     try:
+        print("listening @ svn://localhost:3690 ...")
         server.serve_forever()
     except KeyboardInterrupt:
-        print("\nBye.")
         server.stop()
